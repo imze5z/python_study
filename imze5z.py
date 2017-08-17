1ee0a6be321066d4cc69127a328e8fbc4a950c94
imze5z <imze5z@aliyun.com>
2017-08-17 10:54:21 -0400

    cw3tk6osD5UyJ8OgSuEj

--- imze5z.py
+++ imze5z.py
@@ -11,6 +11,25 @@ import yaml
 from threading import Thread, Lock, current_thread
 import logging
 import traceback
+import time
+
+def func_runtime(callback):
+    now = time.time()
+    print('%s - start: %f' % (callback, now))
+    callback()
+    now = time.time() - now
+    print('%s - spent: %f Second' % (callback, now))
+
+def timestamp_to_time(timestamp):
+    x = time.localtime(timestamp)
+    return time.strftime('%Y-%m-%d %H:%M:%S', x)
+
+def time_to_timestamp(time_str):
+    x = time.strptime(time_str, '%Y-%m-%d %H:%M:%S')
+    return time.mktime(x)
+
+def get_current_timestamp():
+    return time.time()
 
 def random_str(n=20):
     return ''.join(random.sample(string.ascii_letters + string.digits, n))
