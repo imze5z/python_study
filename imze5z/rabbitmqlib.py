797169f12d799d2895c53dd3e06cca67df5b74e7
imze5z <imze5z@aliyun.com>
2017-08-26 05:18:26 -0400

    rabbitmq没有连接池的概念，也不能使用连接池，任何异常的抛出只会导致当前任务失败，但当,rabbitmq连接失效时，只能以重启

--- imze5z/rabbitmqlib.py
+++ imze5z/rabbitmqlib.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import pika
+import traceback
 
 
 class RabbitmqBase(object):
@@ -27,9 +28,13 @@ class _RabbitmqTask(Thread):
         self.properties = properties
 
     def run(self):
-        super().run()
-        if not self.no_ack:
-            self.ch.basic_ack(delivery_tag=self.method.delivery_tag)
+        try:
+            super().run()
+            if not self.no_ack:
+                self.ch.basic_ack(delivery_tag=self.method.delivery_tag)
+        except Exception as e:
+            print(traceback.format_exc())
+            raise e
 
 
 class RabbitmqCustomer(RabbitmqBase):
@@ -51,32 +56,48 @@ class RabbitmqCustomer(RabbitmqBase):
             body=body)
 
     def store_data(self, data):
-        self.ch.basic_publish(
-            exchange='',
-            routing_key=self.store_queue,
-            properties=pika.BasicProperties(delivery_mode=2),
-            body=data)
+        try:
+            self.ch.basic_publish(
+                exchange='',
+                routing_key=self.store_queue,
+                properties=pika.BasicProperties(delivery_mode=2),
+                body=data)
+        except Exception as e:
+            print(traceback.format_exc())
+            raise e
 
     def serv_forever(self, func):
-        if self.task_queue:
-            self.ch.queue_declare(queue=self.task_queue, durable=self.durable)
-        if self.store_queue:
-            self.ch.queue_declare(queue=self.store_queue, durable=self.durable)
+        try:
+            if self.task_queue:
+                self.ch.queue_declare(queue=self.task_queue, durable=self.durable)
+            if self.store_queue:
+                self.ch.queue_declare(queue=self.store_queue, durable=self.durable)
+        except Exception as e:
+            print(traceback.format_exc())
+            raise e
 
         def callback(ch, method, properties, body):
-            if self.prefetch_count > 1:
-                task = _RabbitmqTask(func, (self, body), self.no_ack, ch,
-                                     method, properties)
-                task.start()
-            else:
-                func(self, body)
-                if not self.no_ack:
-                    self.ch.basic_ack(delivery_tag=self.method.delivery_tag)
+            try:
+                if self.prefetch_count > 1:
+                    task = _RabbitmqTask(func, (self, body), self.no_ack, ch,
+                                         method, properties)
+                    task.start()
+                else:
+                    func(self, body)
+                    if not self.no_ack:
+                        self.ch.basic_ack(delivery_tag=self.method.delivery_tag)
+            except Exception as e:
+                print(traceback.format_exc())
+                raise e
 
-        self.ch.basic_qos(prefetch_count=self.prefetch_count)
-        self.ch.basic_consume(
-            callback, queue=self.task_queue, no_ack=self.no_ack)
-        self.ch.start_consuming()
+        try:
+            self.ch.basic_qos(prefetch_count=self.prefetch_count)
+            self.ch.basic_consume(
+                callback, queue=self.task_queue, no_ack=self.no_ack)
+            self.ch.start_consuming()
+        except Exception as e:
+            print(traceback.format_exc())
+            raise e
 
 
 class RabbitmqProducter(RabbitmqBase):
@@ -86,13 +107,21 @@ class RabbitmqProducter(RabbitmqBase):
         self.durable = durable
 
     def send_task(self, body):
-        self.ch.basic_publish(
-            exchange='',
-            routing_key=self.task_queue,
-            properties=pika.BasicProperties(delivery_mode=2),
-            body=body)
+        try:
+            self.ch.basic_publish(
+                exchange='',
+                routing_key=self.task_queue,
+                properties=pika.BasicProperties(delivery_mode=2),
+                body=body)
+        except Exception as e:
+            print(traceback.format_exc())
+            raise e
 
     def produce(self, func):
-        if not self.task_queue:
-            self.ch.queue_declare(queue=self.task_queue, durable=self.durable)
-        func(self)
+        try:
+            if not self.task_queue:
+                self.ch.queue_declare(queue=self.task_queue, durable=self.durable)
+            func(self)
+        except Exception as e:
+            print(traceback.format_exc())
+            raise e
---
+++ imze5z/requestslib.py
@@ -0,0 +1,76 @@
+# -*- encoding: utf-8 -*-
+
+import requests
+from collections import OrderDict
+from requests.utils iport dict_from_cookiejar
+
+
+class HTTP(object):
+    def __init__(self, session=False):
+        if session:
+            self.session = requests.Session()
+
+    def get_session_cookie():
+        if session:
+            return dict_from_cookiejar(self.session.cookies)
+
+    def get(self,
+            url,
+            headers,
+            cookies,
+            timeout=30,
+            verify=False,
+            proxies=None):
+        if self.session:
+            r = self.session.get(url,
+                                 headers=OrderDict(headers),
+                                 cookies=cookies,
+                                 timeout=timeout,
+                                 verify=verify,
+                                 proxies=proxies)
+            r.raise_for_status()
+            r.encoding = 'utf-8'
+            return r.text, r.headers, dict_from_cookiejar(r.cookies), r.history
+        else:
+            r = requests.get(url,
+                             headers=OrderDict(headers),
+                             cookies=cookies,
+                             timeout=timeout,
+                             verify=verify,
+                             proxies=proxies)
+            r.raise_for_status()
+            r.encoding = 'utf-8'
+            return r.text, r.headers, dict_from_cookiejar(r.cookies), r.history
+
+    def post(self,
+             url,
+             headers,
+             cookies,
+             timeout=30,
+             data={},
+             verify=False,
+             proxies=None):
+        if self.session:
+            r = self.session.post(
+                url,
+                headers=OrderDict(headers),
+                cookies=cookies,
+                timeout=timeout,
+                data=data,
+                verify=verify,
+                proxies=proxies)
+            r.raise_for_status()
+            r.encoding = 'utf-8'
+            return r.text, r.headers, dict_from_cookiejar(r.cookies), r.history
+        else:
+            r = requests.post(
+                url,
+                headers=OrderDict(headers),
+                cookies=cookies,
+                timeout=timeout,
+                data=data,
+                verify=verify,
+                proxies=proxies)
+            r.raise_for_status()
+            r.encoding = 'utf-8'
+            return r.text, r.headers, dict_from_cookiejar(r.cookies), r.history
