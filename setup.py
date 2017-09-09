510dfef165e382baccf853a3774beec25e97454e
imze5z <imze5z@aliyun.com>
2017-09-09 23:53:49 +0800

    setup

--- setup.py
+++ setup.py
@@ -1,21 +1,12 @@
 from setuptools import setup, find_packages
 import os
 
-os.system('pip install -r requirements.txt')
-
-scripts = [
-    "excellib.py", "html_parserlib.py", "mongolib.py", "mysqllib.py",
-    "rabbitmqlib.py", "randomlib.py", "timelib.py", "yamllib.py"
-]
-
-scripts = [os.path.sep.join(['imze5z', script]) for script in scripts]
-
 setup(
     name="imze5z",
     version="0.10",
     description="some useful module",
     author="imze5z",
-    url="http://github.com/imze5z/python_study",
+    url="https://github.com/imze5z/python_study/",
     license="LGPL",
-    packages=find_packages(),
-    scripts=scripts)
+    packages=find_packages()
+)
